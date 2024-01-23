bfe483084bf2b24c17f69989619394db8a2fe6ed
Green <green@greenthepear.com>
2024-01-23 13:26:15 +0100

    Small view changes

--- listings/urls.py
+++ listings/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.get_all_cat_listings_json, name="getAllCatsJson"),
-    path("source", views.get_source_json, name="getSourceInfo")
+    path("sources", views.get_source_json, name="getSourceInfo")
 ]
--- listings/views.py
+++ listings/views.py
@@ -9,7 +9,10 @@ def get_all_cat_listings_json(request):
     return HttpResponse(data, content_type="application/json")
 
 def get_source_json(request):
-    source_url=request.GET['url']
-    thatsource = Source.objects.all().filter(url=source_url)
+    source_url = request.GET.get('url', 'all')
+    thatsource = Source.objects.all()
+    if source_url != 'all':
+        thatsource = Source.objects.filter(url=source_url)
+
     data = serialize("json", thatsource)
     return HttpResponse(data, content_type="application/json")
